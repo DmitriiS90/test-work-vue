b2d169d317bb6963cc941f947ab6a04380e3d29a
DmitriiS90 <shegolev1990@yandex.ru>
2021-01-01 20:23:36 +0300

    add vuelidate

--- src/views/CreateProfile.vue
+++ src/views/CreateProfile.vue
@@ -8,17 +8,37 @@
     <form @submit.prevent="submitForm">
 
       <div class="field">
-        <input id="name" v-model="name" type="text" placeholder="name" class="validate" required/>
-        <span class="helper-text" data-error="name is requerd" ></span>
+        <input type="text" 
+               placeholder="name"
+               v-model="name"
+               required
+               :class="{invalid: $v.name.$dirty && !$v.name.required}"/>
+
+        <span v-if="$v.name.$dirty && !$v.name.required" class="helper-text invalid" data-error="Name is required"></span>
       </div>
+
       <div class="field">
-        <input id="email" v-model="email" type="email" placeholder="email" class="validate" required/>
-        <span class="helper-text" data-error="email is requerd" ></span>
+        <input type="email"
+               placeholder="email"
+               v-model="email"
+               required
+               :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"/>
+
+        <span v-if="$v.email.$dirty && !$v.email.required" class="helper-text invalid" data-error="Email is required"></span> 
+        <span v-else-if="$v.email.$dirty && !$v.email.email" class="helper-text invalid" data-error="Invalid email"></span> 
       </div>
+
       <div class="field">
-        <input id="phone" v-model="phoneNumber" type="text" placeholder="phone number" class="validate" required/>
-        <span class="helper-text" data-error="phone number is requerd" ></span>
+        <input  type="text" 
+                placeholder="phone number"
+                v-model="phoneNumber"
+                required
+                :class="{invalid: ($v.phoneNumber.$dirty && !$v.phoneNumber.required) || ($v.phoneNumber.$dirty && !$v.phoneNumber.minLength)}"/>
+
+        <span v-if="$v.phoneNumber.$dirty && !$v.phoneNumber.required" class="helper-text invalid" data-error="phone number is requerd" ></span>
+        <span v-else-if="$v.phoneNumber.$dirty && !$v.phoneNumber.minLength" class="helper-text invalid" data-error="Phone number length must be 8" ></span>
       </div>
+
       <div class="field">
         <select ref="select" v-model="status" class="validate" required>
           <option value="" disabled selected>status</option>
@@ -37,23 +57,30 @@
 </template>
 
 <script>
+import {email, required, minLength} from 'vuelidate/lib/validators'
 
 export default {
-  name: 'CreateUser',
-  components: {
-    
-  },
+  name: 'CreateProfile',
   data:()=>({
     name:'',
     email:'',
     phoneNumber:'',
     status:''
   }),
+  validations: {
+    name:{required},
+    email:{email, required},
+    phoneNumber:{required, minLength: minLength(8)}
+  },
   mounted(){
     M.FormSelect.init(this.$refs.select);
   },
   methods: {
     submitForm(){
+      if(this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
       const user = {
         name:this.name,
         id: Date.now(),
